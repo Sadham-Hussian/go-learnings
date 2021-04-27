7a8ad16e173280ae9a6434f867f797effe62cecb
Sadham-Hussian <sadhamhussian2020@gmail.com>
2021-04-27 18:46:02 +0530

    Add example of using Redis in Golang

---
+++ go-redis/go.mod
@@ -0,0 +1,9 @@
+module github.com/Sadham-Hussian/go-learnings/go-redis
+
+go 1.14
+
+require (
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/go-redis/redis/v8 v8.8.2
+	github.com/joho/godotenv v1.3.0
+)
---
+++ go-redis/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/go-redis/redis/v8"
+)
+
+// Product struct
+type Product struct {
+	ProductID    int    `json:"product_id"`
+	ProductName  string `json:"product_name"`
+	ProductPrize int    `json:"product_prize"`
+}
+
+func main() {
+	fmt.Println("Connecting Golang and Redis")
+
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	pong, err := redisClient.Ping(redisClient.Context()).Result()
+	fmt.Println(pong, err)
+
+	// Setting key value
+	err = redisClient.Set(redisClient.Context(), "name", "jack", 0).Err()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Getting value
+	val, err := redisClient.Get(redisClient.Context(), "name").Result()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(val)
+
+	product, err := json.Marshal(Product{
+		ProductID:    1234,
+		ProductName:  "pen",
+		ProductPrize: 10,
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = redisClient.Set(redisClient.Context(), "1234", product, 0).Err()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	val, err = redisClient.Get(redisClient.Context(), "1234").Result()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(val)
+}
